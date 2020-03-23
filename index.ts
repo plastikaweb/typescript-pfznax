487174cc3f665ac3836a1489da4e65fef07d76b0
plastikaweb <info@plastikaweb.com>
2020-03-23 09:21:10 +0100

    1 file modified, 1 added, 1 deleted

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 function onload() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', './image.jpeg');
-  xhr.responseType = 'arraybuffer';
+  xhr.open("GET", "/image.jpg");
+  xhr.responseType = "arraybuffer";
 
   xhr.onload = function(e) {
-    const blob = new Blob([xhr.response])
+    const blob = new Blob([xhr.response]);
     const url = URL.createObjectURL(blob);
     console.log(url);
 
-    const img: HTMLImageElement = document.getElementById('image')
+
+    const img: HTMLImageElement = document.getElementById("image");
     img.src = url;
-  }
+  };
 
   xhr.send();
 }
